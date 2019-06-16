5a8673a3a2fa9114feecc2cbcc7e13fff3309c3b
rafamilo <camilo.melges@gmail.com>
2019-06-16 01:55:40 -0400

    bootstrap, awesome icons, init navbar, kratos logo and favicon

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar class="NavBar" toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Link</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./router/index";
+import store from "./store/index";
+import BootstrapVue from "bootstrap-vue";
+import "./assets/scss/import.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSearch);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
