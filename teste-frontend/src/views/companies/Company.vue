60fcdb40712cd0a4a98f328969e762f16ef0b42b
Thrashattack <henrique2092@gmail.com>
2019-08-14 20:40:35 -0400

    iniciado jumbotron

--- teste-frontend/src/views/companies/Company.vue
+++ teste-frontend/src/views/companies/Company.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="d-flex row justify-content-center">
+  <div class="d-flex row justify-content-between align-items-start p-4">
+    <img :src="getCurrentCompany.logo" alt="" class="logo rounded-circle col-3" style="height: 200px; width: 200px;">
+    <b-jumbotron :lead="getCurrentCompany.nomeFantasia" bg-variant="white"
+     class="col-12 col-md-8">
+        <span class=""></span>
+    </b-jumbotron>
+
     <span class="col-12 text-center mb-4 mt-4">Documentos</span>
-    <div class="col-12 d-flex flex-wrap p-3 m-0 justify-content-center">
+    <div class="col-12 d-flex flex-wrap p-3 m-0 justify-content-start">
       <DocumentWidget v-for="Document in getDocuments" :key="Document.id"
         :valid="!Document.vencido"
         :title="Document.nome"
